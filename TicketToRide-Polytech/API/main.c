f7f7854136c6ee10399bc263674c546bec1a63bb
harena-rsh <ratsirahonanaharena@yahoo.fr>
2020-12-09 21:33:47 +0100

    test fonction pour remplir les struct OK

--- TicketToRide-Polytech/API/main.c
+++ TicketToRide-Polytech/API/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "clientAPI.h"
 #include "TicketToRideAPI.h"
+#include "main.h"
 
 void affiche_t_objectives(t_objective tab[],int taille){
   for(int i=0;i<taille;i++){
@@ -69,23 +70,40 @@ int main(){
     printf("%d\n",start);
     //affiche(tracks,5*Nb_tracks);
     /*Affiche la map pou voir l'évolution*/
-    printMap();
+    //printMap();
 
     /*DEFINITON DE LA BOUCLE DU JEU*/
     t_return_code coup=0; //On le mets à 0 pour être sûr que dès le début on a 0 et on rentre dans la boucle de jeu.
     t_drawBlindCard blindcard;
-    t_color*card=malloc(sizeof(t_color)*150);
+    t_color*card=malloc(sizeof(t_color));
     t_move move;
     move.type=2;
     move.drawBlindCard= blindcard; //Initialisation de move
     t_move*p_move=&move;
     int replay=0; //bolean pour savoir si l'adversaire rejoue ou pas
     int*p_replay=&replay;
+    t_objective obj[3];
+    int tab_objectif[3]={1,0,0};
 
+    /*Création et initialisation d'une variable t_joueur*/
+    t_joueur moi;
+    t_joueur* p_joueur=&moi;
+    int carte[10]={0};//Comment on le remplit??? avec une fonction comme quand on affiche avec des cas
+    t_objective objectifs[5];
+
+    p_joueur->nb_wagons=45; //45wagons au début du jeu
+    p_joueur->nb_cartes=4; //4 premières cartes distribuées
+    p_joueur->nb_objectifs=0; //3 premiers qu'on nous donne
+    p_joueur->cartes=carte; //tableau de taille 9
+    p_joueur->objectifs=objectifs;
+
+    /*Initialiser cartes avec les 4 premières cartes*/
+
+    affiche_joueur(p_joueur);
       //Tant que ce n'est pas un WINNING_MOVE ou LOOSING_MOVE on reste dans la boucel
       /*On ne fait que tirer deux cartes dans la pioche par tour*/
       /*Pour tirer une carte on utilise drawBlindCard*/
-      while(coup==0){
+      /*while(coup==0){
         coup=drawBlindCard(card);
         coup=drawBlindCard(card);
         coup=getMove(p_move,p_replay);
@@ -96,8 +114,27 @@ int main(){
       }
     else{
       printf("Vous avez perdu!Dommage!\n");
-    }
+    }*/
+    //drawObjectives(obj);
+    //p_joueur->nb_objectifs++;
+    affiche_t_objectives(obj,3);
 
+    initCartes(cards,carte);
+    affiche(carte,10);
+    /*coup=drawBlindCard(card);
+    affiche_t_color(card,1);
+    remplirCartes(carte,card);
+    coup=drawBlindCard(card);
+    affiche_t_color(card,1);
+    remplirCartes(carte,card);
+    affiche(carte,10);*/
+
+    coup=drawObjectives(obj);
+    coup=chooseObjectives(tab_objectif);
+    p_joueur->nb_objectifs++;
+    affiche_t_objectives(obj,3);
+
+    printMap();
     free(tracks);
     free(gameName);
     free(card);
